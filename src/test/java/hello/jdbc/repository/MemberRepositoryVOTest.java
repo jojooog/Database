34f8d87f35387a010c8d7c3f968c3230b2d98a33
jojooog <jooog0417@gmail.com>
2022-06-05 15:19:15 +0900

    HikariDataSource connection pool test

--- src/test/java/hello/jdbc/repository/MemberRepositoryVOTest.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryVOTest.java
@@ -1,19 +1,45 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryVOTest {
 
     MemberRepositoryVO repository = new MemberRepositoryVO();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("member1", 10000);
+        Member member = new Member("member70", 10000);
         repository.save(member);
+
+
+        //id로 조회
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+        //update
+        repository.update(member.getMemberId(), 50000);
+
+        Member updatedMember = repository.findById(member.getMemberId());
+        Assertions.assertThat(updatedMember.getMoney()).isEqualTo(50000);
+
+
+        //delete
+        repository.delete(member.getMemberId());
+        Assertions.assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
+
+
+
 }
