13c3f442e2fddacc0a548b516b1ec7380df8506d
jojooog <jooog0417@gmail.com>
2022-07-06 21:49:52 +0900

    자바 체크예외 throw 테스트

--- src/test/java/hello/jdbc/exception/basic/CheckedTest.java
+++ src/test/java/hello/jdbc/exception/basic/CheckedTest.java
@@ -2,6 +2,7 @@ package hello.jdbc.exception.basic;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
@@ -9,10 +10,20 @@ public class CheckedTest {
 
 
 
+    //exception catch 테스트
     @Test
     void checked_catch(){
         Service service = new Service();
         service.catchException();
+    }
+
+
+    //exception throw 테스트
+    @Test
+    void checked_throw(){
+        Service service = new Service();
+
+        Assertions.assertThatThrownBy(() -> service.throwException()).isInstanceOf(MyCheckedException.class);
     }
 
 
@@ -30,12 +41,18 @@ public class CheckedTest {
         Repository repository = new Repository();
 
 
+        //체크예외 catch
         public void catchException(){
             try {
                 repository.callException();
             } catch (MyCheckedException e) {
                 log.info("예외 처리, message={}", e.getMessage(), e);
             }
+        }
+
+        //체크예외 throw
+        public void throwException() throws MyCheckedException {
+            repository.callException();
         }
 
     }
